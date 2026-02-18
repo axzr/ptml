f889e8ee9ebed91da3861ee550d90ee62d2f7bf3
axzr <ad@mrbrooks.org>
2026-02-18 13:06:06 +0000

    fix: update base path in Vite configuration to root for deployment to js.org, added CNAME file.

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
 export default defineConfig({
-  base: '/ptml/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
